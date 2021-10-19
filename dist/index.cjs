7c99d2293cc9428459f360de9b10a922c5266cd4
Josep M Sobrepere <jm.sobrepere@gmail.com>
2021-10-19 09:27:48 +0200

    fix: dist files

--- dist/index.cjs
+++ dist/index.cjs
@@ -1,7 +1,7 @@
 "use strict"
 
 if (process.env.NODE_ENV === "production") {
-  module.exports = require("./scale-codec.cjs.production.min.js")
+  module.exports = require("./rxjs-state.cjs.production.min.js")
 } else {
-  module.exports = require("./scale-codec.cjs.development.js")
+  module.exports = require("./rxjs-state.cjs.development.js")
 }
