bf6b7a5d9d4dead1cc826e1e3bd12ff1038781bb
Josep M Sobrepere <jm.sobrepere@gmail.com>
2022-04-01 01:23:10 +0200

    @rxstate/core@0.0.1

--- dist/index.js
+++ dist/index.js
@@ -1,7 +1,7 @@
 "use strict"
 
 if (process.env.NODE_ENV === "production") {
-  module.exports = require("./rxjs-state.cjs.production.min.js")
+  module.exports = require("./rxstate.core.cjs.production.min.js")
 } else {
-  module.exports = require("./rxjs-state.cjs.development.js")
+  module.exports = require("./rxstate.core.cjs.development.js")
 }
